ba738c5e45bf05cb157df27a1566046b3466945d
Gabriel Luque <gluque@uma.es>
2019-03-22 11:44:22 +0100

    Primera versión completa de mac-analyzer

--- tools/mac-analyzer.py
+++ tools/mac-analyzer.py
@@ -1,7 +1,28 @@
 import sys
 sys.path.append('..')
-
 from utils import maclib
+
+
+def print_report(mac):
+    print(f"Complete MAC: {mac}")
+    print("\tVendor:")
+    print(f"\t\tID: {mac.get_vendor_id()}")
+    vendor = mac.get_vendor_name()
+    if vendor is not None:
+        print(f"\t\tName: {vendor}")
+    print(f"\tSerial Number: {mac.get_serial_number()}")
+    if mac.is_local():
+        print("\tThis address is locally administered")
+    else:
+        print("\tThis address is global")
+    print("\tThis address is ",end="")
+    if mac.is_unicast():
+        print("unicast")
+    elif mac.is_broadcast():
+        print("broadcast")
+    else:
+        print("multicast")
+
 
 if __name__ == "__main__":
 
@@ -11,6 +32,7 @@ if __name__ == "__main__":
     else:
         try:
             mac = maclib.MACAddress(sys.argv[1])
+            print_report(mac)
         except maclib.MACAddressException:
             print(f"{sys.argv[1]} is not a valid MAC")
 
