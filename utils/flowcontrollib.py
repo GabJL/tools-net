da3e6f1929913e64920ef5c5668568903d5f2fdd
Gabriel Luque <gluque@uma.es>
2019-04-13 17:18:56 +0200

    Go-Back-N seems to works. Next protocol: SR

--- utils/flowcontrollib.py
+++ utils/flowcontrollib.py
@@ -1,8 +1,8 @@
-from enum import Enum
+from enum import IntEnum
 import json
 
 
-class Event(Enum):
+class Event(IntEnum):
     RECEIVE_ACK = 1
     RECEIVE_NACK = 2
     TRANS_FRAME_END = 3
@@ -27,7 +27,7 @@ class EventQueue:
         self.queue = [ev] + self.queue
 
     def next_event(self):
-        if self.queue is None:
+        if self.queue is None or len(self.queue) == 0:
             return None
 
         pos_ev = 0
@@ -35,7 +35,7 @@ class EventQueue:
             if self.queue[pos_ev]["time"] > self.queue[x]["time"]:
                 pos_ev = x
             elif self.queue[pos_ev]["time"] == self.queue[x]["time"]:
-                if self.queue[pos_ev]["type"] < self.queue[x]["type"]:
+                if self.queue[pos_ev]["type"] > self.queue[x]["type"]:
                     pos_ev = x
 
         ev = self.queue[pos_ev]
@@ -43,7 +43,7 @@ class EventQueue:
         return ev
 
     def remove_timeout(self, seq_number=-1):
-        for i, ev in enumerate(self.queue):
+        for i , ev in enumerate(self.queue):
             if ev["type"] == Event.TIMEOUT and (seq_number == -1 or ev["seq_number"] == seq_number):
                 del self.queue[i]
 
@@ -62,6 +62,10 @@ class EventQueue:
             if ev["type"] == Event.SEND_FRAME:
                 ev["time"] = next_time
 
+    def set_send_current_time(self, t):
+        for ev in self.queue:
+            if ev["type"] == Event.SEND_FRAME:
+                ev["time"] = t
 
 class ProtocolError(Exception):
     pass
@@ -80,25 +84,25 @@ class Protocol:
                 raise ProtocolError(f"The number of bits for numbering the frames should be greater than 1.")
             if info.get("number of frames", 0) < 1:
                 raise ProtocolError(f"The number of frames to be sent should be greater than 1.")
-            if info.get("frames lost", []) is not list:
+            if type(info.get("frames lost", [])) is not list:
                 raise ProtocolError("The list of frames lost should be a list, even if only one frame is lost.")
             else:
                 for v in info.get("frames lost", []):
                     if v < 1:
                         raise ProtocolError("The lost frames should be numbered as 1, 2...")
-            if info.get("acks lost", []) is not list:
+            if type(info.get("acks lost", [])) is not list:
                 raise ProtocolError("The list of acks lost should be a list, even if only one is lost.")
             else:
                 for v in info.get("ack lost", []):
                     if v < 1:
                         raise ProtocolError("The lost acks should be numbered as 1, 2...")
 
-            for k, v in info.items:
-                if "time" in k and v is not list and v < 0:
+            for k, v in info.items():
+                if "time" in k and type(v) is not list and v < 0:
                     raise ProtocolError(f"{k} should be >= 0")
 
             self.__set_default_values()
-            for k, v in info.items:
+            for k, v in info.items():
                 self.configuration[k] = v
 
             self.max_number = 2 ** self.configuration["bit for numbering"]
@@ -142,6 +146,7 @@ class Protocol:
         self.acks_sent = 0
         self.queue = EventQueue()
         self.sender_window_start = 0
+        self.log = None
 
     def __update_sender_window(self, n):
         for i in self.numbering[self.sender_window_start:self.sender_window_size + self.sender_window_start]:
@@ -154,9 +159,9 @@ class Protocol:
         for i, v in enumerate(self.numbering):
             if not self.receiver_window[i] and first is None:
                 first = i
-            if first and (i - first) < self.receiver_window_size and v == n:
-                return True, i == first
-        return False, False
+            if first is not None and (i - first) < self.receiver_window_size and v == n:
+                return True, i == first, self.receiver_window[first]
+        return False, False, 0
 
     def __update_receiver_window(self, n):
         for i, v in enumerate(self.numbering):
@@ -170,37 +175,68 @@ class Protocol:
                 return i
         return None
 
+    def __sender_window_to_str(self):
+        res = "["
+        for i in range(self.sender_window_start, self.sender_window_start+self.sender_window_size):
+            if i < len(self.sender_window):
+                if self.sender_window[i]:
+                    res += f" ({self.numbering[i]})"
+                else:
+                    res += f" {self.numbering[i]}"
+        res += " ]"
+        return res
+
+    def __receiver_window_to_str(self):
+        res = "["
+        first = None
+        for i, v in enumerate(self.numbering):
+            if not self.receiver_window[i] and first is None:
+                first = i
+            if first is not None and (i - first) < self.receiver_window_size:
+                if self.receiver_window[i]:
+                    res += f" ({self.numbering[i]})"
+                else:
+                    res += f" {self.numbering[i]}"
+        res += " ]"
+        return res
+
     def run(self):
         log = []
-        for i in range(self.sender_window_size):
+        for i in range(len(self.numbering)):
             self.queue.add_event(Event.SEND_FRAME, 0, self.numbering[i])
+
+        log.append({"time": 0, "entity": 'sender', "type": "Initial State", "number": -1,
+                    "window": self.__sender_window_to_str()})
+        log.append({"time": 0, "entity": 'receiver', "type": "Initial State", "number": -1,
+                    "window": self.__receiver_window_to_str()})
 
         while True:
             ev = self.queue.next_event()
-            if ev is None:
+            if ev is not None:
                 if ev["type"] == Event.SEND_FRAME:
                     if ev["seq_number"] in self.numbering[
                                        self.sender_window_start:self.sender_window_size + self.sender_window_start]:
                         next_time = ev["time"] + self.configuration["frame transmission time"]
                         self.queue.add_event(Event.TRANS_FRAME_END, next_time, ev["seq_number"])
                         log.append({"time": ev["time"], "entity": 'sender', "type": "Start to send Frame",
-                                    "number": ev["seq_number"]})
+                                    "number": ev["seq_number"], "window": self.__sender_window_to_str()})
+                    else:
+                        self.queue.add_event_front(ev["type"], ev["time"], ev["seq_number"])
                     self.queue.move_send_next_event()
                 elif ev["type"] == Event.TRANS_FRAME_END:
                     self.queue.add_event(Event.TIMEOUT, ev["time"] + self.configuration["timeout"], ev["seq_number"])
-                    log.append({"time": ev["time"], "entity": 'sender', "type": "Frame completely sent",
-                                "number": ev["seq_number"]})
                     self.frames_sent += 1
                     self.__update_sender_window(ev["seq_number"])
-                    if ev["seq_number"] in self.configuration["frames lost"]:
+                    log.append({"time": ev["time"], "entity": 'sender', "type": "Frame completely sent",
+                                "number": ev["seq_number"], "window": self.__sender_window_to_str()})
+                    if self.frames_sent in self.configuration["frames lost"]:
                         log.append({"time": ev["time"], "entity": 'sender', "type": "Frame lost",
-                                    "number": ev["seq_number"]})
+                                    "number": ev["seq_number"], "window": self.__sender_window_to_str()})
                     else:
                         self.queue.add_event(Event.RECEIVE_FRAME,
                                              ev["time"] + self.configuration["frame propagation time"],
                                              ev["seq_number"])
                 elif ev["type"] == Event.TIMEOUT:
-                    log.append({"time": ev["time"], "entity": 'sender', "type": "Timeout", "number": ev["seq_number"]})
                     if self.configuration["protocol"] == "Go-Back-N":
                         for i in range(self.sender_window_start + self.sender_window_size-1,
                                        self.sender_window_start-1, -1):
@@ -211,55 +247,89 @@ class Protocol:
                     else:
                         self.queue.add_event_front(Event.SEND_FRAME, ev["time"], ev["seq_number"])
                         self.__update_sender_window(ev["seq_number"])
+                    log.append({"time": ev["time"], "entity": 'sender', "type": "Timeout", "number": ev["seq_number"],
+                                "window": self.__sender_window_to_str()})
                 elif ev["type"] == Event.RECEIVE_FRAME:
                     next_time = ev["time"] + self.configuration["processing time"]
                     self.queue.add_event(Event.PROC_ACK_TIME, next_time, ev["seq_number"])
-                    log.append({"time": ev["time"], "receiver": 'receiver', "type": "Frame received",
-                                "number": ev["seq_number"]})
+                    log.append({"time": ev["time"], "entity": 'receiver', "type": "Frame received",
+                                "number": ev["seq_number"], "window": self.__receiver_window_to_str()})
                 elif ev["type"] == Event.PROC_ACK_TIME:
-                    is_in, first = self.__is_in_receiver_window(ev["seg_number"])
+                    is_in, first, val = self.__is_in_receiver_window(ev["seq_number"])
                     if is_in:
-                        next_time = ev["time"] + self.configuration["ack transmis time"]
+                        next_time = ev["time"] + self.configuration["ack transmission time"]
                         if first:
                             self.queue.add_event(Event.TRANS_ACK_END, next_time, ev["seq_number"])
-                            log.append({"time": ev["time"], "receiver": 'receiver', "type": "Start to send ACK",
-                                        "number": ev["seq_number"]})
+                            log.append({"time": ev["time"], "entity": 'receiver', "type": "Start to send ACK",
+                                        "number": ev["seq_number"]+1, "window": self.__receiver_window_to_str()})
                         else:
-                            self.queue.add_event(Event.TRANS_NACK_END, next_time, ev["seq_number"])
-                            log.append({"time": ev["time"], "receiver": 'receiver', "type": "Start to send NACK",
-                                        "number": ev["seq_number"]})
+                            self.queue.add_event(Event.TRANS_NACK_END, next_time, val)
+                            log.append({"time": ev["time"], "entity": 'receiver', "type": "Start to send NACK",
+                                        "number": val, "window": self.__receiver_window_to_str()})
                 elif ev["type"] == Event.TRANS_ACK_END:
                     self.__update_receiver_window(ev["seq_number"])
-                    next_time = ev["time"] + self.configuration["propagation time"]
+                    next_time = ev["time"] + self.configuration["ack propagation time"]
                     self.acks_sent += 1
                     if self.acks_sent in self.configuration["acks lost"]:
-                        log.append({"time": ev["time"], "receiver": 'receiver', "type": "ACK lost",
-                                    "number": ev["seq_number"]})
+                        log.append({"time": ev["time"], "entity": 'receiver', "type": "ACK lost",
+                                    "number": ev["seq_number"]+1, "window": self.__receiver_window_to_str()})
                     else:
                         self.queue.add_event(Event.RECEIVE_ACK, next_time, ev["seq_number"])
-                        log.append({"time": ev["time"], "receiver": 'receiver', "type": "ACK sent",
-                                    "number": ev["seq_number"]})
+                        log.append({"time": ev["time"], "entity": 'receiver', "type": "ACK sent",
+                                    "number": ev["seq_number"]+1, "window": self.__receiver_window_to_str()})
                 elif ev["type"] == Event.TRANS_NACK_END:
                     self.__update_receiver_window(ev["seq_number"])
-                    next_time = ev["time"] + self.configuration["propagation time"]
+                    next_time = ev["time"] + self.configuration["ack propagation time"]
                     self.acks_sent += 1
                     if self.acks_sent in self.configuration["acks lost"]:
-                        log.append({"time": ev["time"], "receiver": 'receiver', "type": "NACK lost",
-                                    "number": ev["seq_number"]})
+                        log.append({"time": ev["time"], "entity": 'receiver', "type": "NACK lost",
+                                    "number": ev["seq_number"], "window": self.__receiver_window_to_str()})
                     else:
                         self.queue.add_event(Event.RECEIVE_NACK, next_time, ev["seq_number"])
-                        log.append({"time": ev["time"], "receiver": 'receiver', "type": "NACK sent",
-                                    "number": ev["seq_number"]})
-                    pass
+                        log.append({"time": ev["time"], "entity": 'receiver', "type": "NACK sent",
+                                    "number": ev["seq_number"], "window": self.__receiver_window_to_str()})
                 elif ev["type"] == Event.RECEIVE_ACK:
                     pos = self.__get_position(ev["seq_number"])
-                    if pos:
+                    if pos is not None:
+                        for i in range(self.sender_window_start, pos+1):
+                            self.queue.remove_timeout(self.numbering[i])
                         self.sender_window_start = pos+1
-                        log.append({"time": ev["time"], "receiver": 'sender', "type": "ACK received",
-                                    "number": ev["seq_number"]})
+                        log.append({"time": ev["time"], "entity": 'sender', "type": "ACK received",
+                                    "number": ev["seq_number"]+1, "window": self.__sender_window_to_str()})
+                        self.queue.set_send_current_time(ev["time"])
                 elif ev["type"] == Event.RECEIVE_NACK:
                     self.queue.remove_timeout(ev["seq_number"])
                     self.queue.add_event_front(Event.SEND_FRAME, ev["time"], ev["seq_number"])
-                    log.append({"time": ev["time"], "receiver": 'sender', "type": "NACK received - retransmit",
-                                "number": ev["seq_number"]})
+                    log.append({"time": ev["time"], "entity": 'sender', "type": "NACK received - retransmit",
+                                "number": ev["seq_number"], "window": self.__sender_window_to_str()})
+            else:
                 break
+        self.log = log
+
+    def write(self):
+        if not self.log:
+            return
+
+        print('{0:<6} {1:<20} {2:<30} {3:>30} {4:<20}'.format("time", "Sender Window", "Sender Action",
+                                                              "Receiver Activon", "Receiver Window"))
+        for e in self.log:
+            if 'NACK' in e['type']:
+                seq = f" (NACK{str(e['number'])})"
+            elif 'ACK' in e['type']:
+                seq = f" (ACK{str(e['number'])})"
+            else:
+                if e['number'] == -1:
+                    seq = ''
+                else:
+                    seq = f" (T{str(e['number'])})"
+
+            if e['entity'] == 'sender':
+                print(f"{e['time']:<6} {e['window']:<20} {e['type'] + seq:<30}")
+            else:
+                print(f"{e['time']:<6} {'':<20} {'':<30} {e['type'] + seq:>30} {e['window']:<20} ")
+
+
+if __name__=="__main__":
+    prot = Protocol('example-prot.json')
+    prot.run()
+    prot.write()
